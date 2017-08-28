9bc2127148874f67545bc6903444c4c0fa999ec1
Andreas Bresser <self@andreasbresser.de>
2017-08-28 15:50:22 +0200

    initial version: load, parse and save basic building, storey and space information

---
+++ README.md
@@ -0,0 +1,3 @@
+# IFC model
+helper classes to load and store data from IFC into JSON for easier access across different platforms.
+
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='ifc_model',
+    author='Andreas Bresser',
+    author_email='self@andreasbresser.de',
+    url='https://github.com/brean/ifc_model',
+    long_description='',
+    version='dev',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[])
