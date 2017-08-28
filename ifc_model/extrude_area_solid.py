88ad1ac30dfe82207d7d4494c6ee0465de42b134
Andreas Bresser <self@andreasbresser.de>
2017-08-28 19:21:32 +0200

    use rotation to position spaces

--- ifc_model/extrude_area_solid.py
+++ ifc_model/extrude_area_solid.py
@@ -23,18 +23,29 @@ class ExtrudedAreaSolid(RepresentationItem):
     def from_ifc(self, ifc_data):
         assert ifc_data.is_a('IfcExtrudedAreaSolid')
         super(ExtrudedAreaSolid, self).from_ifc(ifc_data)
+        # TODO: ifc_data.Position is a Axis2Placement3D, maybe get an own class?
         self.location = ifc_data.Position.Location.Coordinates
+        self.direction = [0, 0, 0]
+        if ifc_data.Position.RefDirection:
+            self.direction = ifc_data.Position.RefDirection.DirectionRatios
         area_type = self.ifc_data.SweptArea.is_a()
+        self.depth = self.ifc_data.Depth
         self.area = self.area_from_class(area_type[3:])
         self.area.from_ifc(self.ifc_data.SweptArea)
 
     def from_json(self, data):
         super(ExtrudedAreaSolid, self).from_json(data)
         self.area = self.area_from_class(data['area']['type'])
+        self.depth = data['depth']
+        self.location = data['location']
+        self.direction = data['direction']
         self.area.from_json(data['area'])
 
     def to_json(self):
         data = super(ExtrudedAreaSolid, self).to_json()
         data['type'] = self.type
+        data['depth'] = self.depth
+        data['location'] = self.location
+        data['direction'] = self.direction
         data['area'] = self.area.to_json()
         return data
